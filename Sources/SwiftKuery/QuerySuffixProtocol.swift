fc3cf1b545d065438250777bdfe11adcab74e5c1
Sergey Minakov <s.minakov@be-interactive.ru>
2017-01-18 16:11:24 +0300

    review changes

--- Sources/SwiftKuery/QuerySuffixProtocol.swift
+++ Sources/SwiftKuery/QuerySuffixProtocol.swift
@@ -1,5 +1,5 @@
 /**
- Copyright IBM Corporation 2016, 2017
+ Copyright IBM Corporation 2017
  
  Licensed under the Apache License, Version 2.0 (the "License");
  you may not use this file except in compliance with the License.
@@ -17,7 +17,6 @@
 // MARK: QuerySuffixProtocol
 
 /// Defines the protocol which should be used for all suffix clauses.
-/// Represents a suffix as String value.
 public protocol QuerySuffixProtocol: Buildable {
     
 }
