d97a1e3128d4217316f63d318cce221c54c8b035
Ira Rosen <IRAR@il.ibm.com>
2017-03-14 13:20:03 +0200

    IBM-Swift/Swift-Kuery#16 Connection pooling

---
+++ Sources/SwiftKuery/ConnectionPoolConnection.swift
@@ -0,0 +1,225 @@
+/**
+ Copyright IBM Corporation 2017
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+// MARK: ConnectionPoolConnection
+
+/// A wrapper around actual `Connection`. Allows seemless release of the connection.
+public class ConnectionPoolConnection: Connection {
+    
+    private var connection: Connection?
+    private weak var pool: ConnectionPool?
+    
+    init(connection: Connection, pool: ConnectionPool) {
+        self.connection = connection
+        self.pool = pool
+    }
+    
+    deinit {
+        if let connection = connection {
+            pool?.release(connection: connection)
+        }
+    }
+    
+    /// Establish a connection with the database.
+    ///
+    /// - Parameter onCompletion: The function to be called when the connection is established.
+    public func connect(onCompletion: (QueryError?) -> ()) {
+        if let _ = connection {
+            onCompletion(nil)
+        }
+        else {
+            if let newConnection = pool?.getConnection() {
+                connection = newConnection
+                onCompletion(nil)
+            }
+            else {
+                onCompletion(.connection("Failed to get connection"))
+            }
+        }
+    }
+    
+    /// Close the connection to the database.
+    public func closeConnection() {
+        if let connection = connection {
+            pool?.release(connection: connection)
+            self.connection = nil
+        }
+    }
+    
+    /// An indication whether there is a connection to the database.
+    public var isConnected: Bool {
+        return connection != nil
+    }
+    
+    /// Execute a query.
+    ///
+    /// - Parameter query: The query to execute.
+    /// - Parameter onCompletion: The function to be called when the execution of the query has completed.
+    public func execute(query: Query, onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.execute(query: query, onCompletion: onCompletion)
+    }
+    
+    /// Execute a raw query.
+    ///
+    /// - Parameter query: A String with the query to execute.
+    /// - Parameter onCompletion: The function to be called when the execution of the query has completed.
+    public func execute(_ raw: String, onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.execute(raw, onCompletion: onCompletion)
+    }
+    
+    /// Execute a query with parameters.
+    ///
+    /// - Parameter query: The query to execute.
+    /// - Parameter parameters: An array of the parameters.
+    /// - Parameter onCompletion: The function to be called when the execution of the query has completed.
+    public func execute(query: Query, parameters: [Any?], onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.execute(query: query, parameters: parameters, onCompletion: onCompletion)
+    }
+    
+    /// Execute a raw query with parameters.
+    ///
+    /// - Parameter query: A String with the query to execute.
+    /// - Parameter parameters: An array of the parameters.
+    /// - Parameter onCompletion: The function to be called when the execution of the query has completed.
+    public func execute(_ raw: String, parameters: [Any?], onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.execute(raw, parameters: parameters, onCompletion: onCompletion)
+    }
+    
+    /// Execute a query with parameters.
+    ///
+    /// - Parameter query: The query to execute.
+    /// - Parameter parameters: A dictionary of the parameters with parameter names as the keys.
+    /// - Parameter onCompletion: The function to be called when the execution of the query has completed.
+    public func execute(query: Query, parameters: [String:Any?], onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.execute(query: query, parameters: parameters, onCompletion: onCompletion)
+    }
+    
+    /// Execute a raw query with parameters.
+    ///
+    /// - Parameter query: A String with the query to execute.
+    /// - Parameter parameters: A dictionary of the parameters with parameter names as the keys.
+    /// - Parameter onCompletion: The function to be called when the execution of the query has completed.
+    public func execute(_ raw: String, parameters: [String:Any?], onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.execute(raw, parameters: parameters, onCompletion: onCompletion)
+    }
+    
+    /// Return a String representation of the query.
+    ///
+    /// - Parameter query: The query.
+    /// - Returns: A String representation of the query.
+    /// - Throws: QueryError.syntaxError if query build fails.
+    public func descriptionOf(query: Query) throws -> String {
+        if let connection = connection {
+            return try connection.descriptionOf(query: query)
+        }
+        else {
+            throw QueryError.connection("No connection to the database")
+        }
+    }
+    
+    /// Start a transaction.
+    ///
+    /// - Parameter onCompletion: The function to be called when the execution of start transaction command has completed.
+    public func startTransaction(onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.startTransaction(onCompletion: onCompletion)
+    }
+    
+    /// Commit the current transaction.
+    ///
+    /// - Parameter onCompletion: The function to be called when the execution of commit transaction command has completed.
+    public func commit(onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.commit(onCompletion: onCompletion)
+    }
+    
+    /// Rollback the current transaction.
+    ///
+    /// - Parameter onCompletion: The function to be called when the execution of rolback transaction command has completed.
+    public func rollback(onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.rollback(onCompletion: onCompletion)
+    }
+    
+    /// Create a savepoint.
+    ///
+    /// - Parameter savepoint: The name to  be given to the created savepoint.
+    /// - Parameter onCompletion: The function to be called when the execution of create savepoint command has completed.
+    public func create(savepoint: String, onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.create(savepoint: savepoint, onCompletion: onCompletion)
+    }
+    
+    /// Rollback the current transaction to the specified savepoint.
+    ///
+    /// - Parameter to savepoint: The name of the savepoint to rollback to.
+    /// - Parameter onCompletion: The function to be called when the execution of rolback transaction command has completed.
+    public func rollback(to savepoint: String, onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.rollback(to: savepoint, onCompletion: onCompletion)
+    }
+    
+    /// Release a savepoint.
+    ///
+    /// - Parameter savepoint: The name of the savepoint to release.
+    /// - Parameter onCompletion: The function to be called when the execution of release savepoint command has completed.
+    public func release(savepoint: String, onCompletion: @escaping ((QueryResult) -> ())) {
+        guard let connection = connection else {
+            onCompletion(.error(QueryError.connection("Connection is disconnected")))
+            return
+        }
+        connection.release(savepoint: savepoint, onCompletion: onCompletion)
+    }
+}
---
+++ Sources/SwiftKuery/ConnectionPoolOptions.swift
@@ -0,0 +1,43 @@
+/**
+ Copyright IBM Corporation 2017
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+
+// MARK: ConnectionPoolOptions
+
+/// Options for `ConectionPool` configuration.
+public struct ConnectionPoolOptions {
+    /// The initial number of connections in the pool.
+    public let initialCapacity: Int
+    
+    /// The maximum number of connections in the pool. The pool is allowed to grow from `initialCapacity` up to
+    /// this limit. If not specified, or `maxCapacity` <= `initialCapacity`, the pool cannot grow.
+    public let maxCapacity: Int
+    
+    /// Maximum wait (in milliseconds) to receive a connection before returning nil. If set to 0 (default), there will be
+    /// no timeout, i.e., the wait will block forever.
+    public let timeout: Int
+    
+    /// Initialize an instance of `ConnectionPoolOptions`.
+    ///
+    /// Parameter initialCapacity: The initial number of connections in the pool.
+    /// Parameter maxCapacity: The maximum number of connections in the pool
+    /// Parameter timeout: Maximum wait (in milliseconds) to receive a connection before returning nil.
+    public init(initialCapacity: Int, maxCapacity: Int = 0, timeout: Int = 0) {
+        self.initialCapacity = initialCapacity
+        self.maxCapacity = maxCapacity
+        self.timeout = timeout
+    }
+}
