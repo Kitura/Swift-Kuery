dcf8cff54285991cbbae156aa9097204f7eeab14
Ira Rosen <irar@il.ibm.com>
2017-05-11 11:21:28 +0300

    Compare Mac vs Linux test count. Randomize test order on Linux. (#74)
    * Add test for Mac vs Linux test count. Randomize test order on Linux. IBM-Swift/Kitura#1056
    
    * IBM-Swift/Kitura#1056 Move shuffle to LinuxMain.swift, move test count to VerifyLinuxTestCount

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,20 +1,62 @@
+/**
+ * Copyright IBM Corporation 2017
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
 import XCTest
+import Glibc
 @testable import SwiftKueryTests
 
+srand(UInt32(time(nil)))
+
+// http://stackoverflow.com/questions/24026510/how-do-i-shuffle-an-array-in-swift
+extension MutableCollection where Indices.Iterator.Element == Index {
+    mutating func shuffle() {
+        let c = count
+        guard c > 1 else { return }
+        
+        for (firstUnshuffled, unshuffledCount) in zip(indices, stride(from: c, to: 1, by: -1)) {
+            let d: IndexDistance = numericCast(random() % numericCast(unshuffledCount))
+            guard d != 0 else { continue }
+            let i = index(firstUnshuffled, offsetBy: d)
+            swap(&self[firstUnshuffled], &self[i])
+        }
+    }
+}
+
+extension Sequence {
+    func shuffled() -> [Iterator.Element] {
+        var result = Array(self)
+        result.shuffle()
+        return result
+    }
+}
+
 XCTMain([
-     testCase(TestAggregateFunctions.allTests),
-     testCase(TestAlias.allTests),
-     testCase(TestFilterAndHaving.allTests),
-     testCase(TestInsert.allTests),
-     testCase(TestJoin.allTests),
-     testCase(TestParameters.allTests),
-     testCase(TestQueryResult.allTests),
-     testCase(TestRaw.allTests),
-     testCase(TestSchema.allTests),
-     testCase(TestSelect.allTests),
-     testCase(TestSpecialOperators.allTests),
-     testCase(TestSubqueries.allTests),
-     testCase(TestSubquery.allTests),
-     testCase(TestSyntaxError.allTests),
-     testCase(TestUpdate.allTests),
-])
+    testCase(TestAggregateFunctions.allTests.shuffled()),
+    testCase(TestAlias.allTests.shuffled()),
+    testCase(TestFilterAndHaving.allTests.shuffled()),
+    testCase(TestInsert.allTests.shuffled()),
+    testCase(TestJoin.allTests.shuffled()),
+    testCase(TestParameters.allTests.shuffled()),
+    testCase(TestQueryResult.allTests.shuffled()),
+    testCase(TestRaw.allTests.shuffled()),
+    testCase(TestSchema.allTests.shuffled()),
+    testCase(TestSelect.allTests.shuffled()),
+    testCase(TestSpecialOperators.allTests.shuffled()),
+    testCase(TestSubqueries.allTests.shuffled()),
+    testCase(TestSubquery.allTests.shuffled()),
+    testCase(TestSyntaxError.allTests.shuffled()),
+    testCase(TestUpdate.allTests.shuffled()),
+    ])
