34b6670a2d312ac21676b68acc3ee23b13d70048
ShihabMehboob <shihab.mehboob1@ibm.com>
2018-04-09 09:35:47 +0100

    Change to logic

--- Sources/SwiftKuery/ConnectionPool.swift
+++ Sources/SwiftKuery/ConnectionPool.swift
@@ -128,6 +128,7 @@ public class ConnectionPool {
         // Check if the item is alive, and append a new one to the pool if it isn't
         if item.isConnected == false {
             releaser(item)
+            capacity -= 1
             if let newItem = generator() {
                 capacity += 1
                 pool.append(newItem)
