94f04e06160e054a31d67cc0c93829fe51002f96
Danny Sung <danny@dannysung.com>
2020-10-04 21:15:44 -0700

    Update package reference to community versions (#198)

--- Package@swift-4.swift
+++ Package@swift-4.swift
@@ -30,7 +30,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.7.0"),
+        .package(url: "https://github.com/Kitura/LoggerAPI.git", from: "1.9.200"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target defines a module or a test suite.
